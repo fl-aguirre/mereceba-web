87bce3d93cd4065cbe1b3d945cc4c5b883dd0345
Robert Flores <rrfloresgonzalez@gmail.com>
2021-07-24 21:46:01 -0300

    nueva rama con los cambios en productos y tips

---
+++ js/mateTipsToggle.js
@@ -0,0 +1,11 @@
+
+
+$("#matetip1").click(()=> {
+    $("#tip1").toggle("fast");
+    $("#tip2").hide("fast");
+})
+
+$("#matetip2").click(()=> {
+    $("#tip2").toggle("fast");
+    $("#tip1").hide("fast");
+})
