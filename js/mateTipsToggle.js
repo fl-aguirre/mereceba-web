0896c3716e766e4f945829cd85cdfd92adfe05dd
Robert Flores <rrfloresgonzalez@gmail.com>
2021-07-30 00:56:46 -0300

    se estuvo trabajando sobre el rediseño de los mate tips, en productos se agregó hover y tamaño del modal title

--- js/mateTipsToggle.js
+++ js/mateTipsToggle.js
@@ -1,11 +1,38 @@
 
 
 $("#matetip1").click(()=> {
-    $("#tip1").toggle("fast");
+    $("#tip1").show("fast");
     $("#tip2").hide("fast");
+    $("#tip3").hide("fast");
+    $("#matetip2").hide("fast");
+    $("#matetip3").hide("fast");
+    
 })
 
 $("#matetip2").click(()=> {
-    $("#tip2").toggle("fast");
+    $("#tip2").show("fast");
     $("#tip1").hide("fast");
-})
+    $("#tip3").hide("fast");
+    $("#matetip1").hide("fast");
+    $("#matetip3").hide("fast");
+    
+});
+
+$("#matetip3").click(()=> {
+    $("#tip3").show("fast");
+    $("#tip2").hide("fast");
+    $("#tip1").hide("fast");
+    $("#matetip2").hide("fast");
+    $("#matetip1").hide("fast");    
+});
+
+// BOTÓN PARA CERRAR EL MATE TIP DESPLEGADO
+$(".tipDesplegado__cerrar").click(()=> {
+    $("#tip1").hide("fast");
+    $("#tip2").hide("fast");
+    $("#tip3").hide("fast");
+    $("#matetip1").show("fast");
+    $("#matetip2").show("fast");
+    $("#matetip3").show("fast");   
+});
+
