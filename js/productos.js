46269ac0a2136cf6eea7b539bccbeed04801b4d0
Franco Aguirre <frl.aguirre@gmail.com>
2021-08-10 18:03:53 -0300

    Cambios en responsive de  letras, logo, carousel e ícono de instagram. Render de imágenes

--- js/productos.js
+++ js/productos.js
@@ -25,41 +25,27 @@ function renderCard(listado) {
     for (const producto of listado) {
         $("#productos").append(`
         <div class="card productos__card--hover" style="width: 18rem;">
-
           <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#modal${producto.id}">
             <img src=${producto.img} class="card-img-top" alt=${producto.nombre}>      
           </button>
-
           <div class="card-body">
             <h5 class="card-title">${producto.nombre}</h5>    
           </div>
-
        </div>
-
        <div class="modal fade" id="modal${producto.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-
-       <div class="modal-dialog modal-dialog-centered">
-
-         <div class="modal-content">
-
-           <div class="modal-header">
-           
-             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-             <h5 class="modal-title" id="${producto.id}ModalLabel">${producto.nombre}</h5>
-             
-
-           </div>
-
-           <div class="modal-body">
-
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h5 class="modal-title" id="${producto.id}ModalLabel">${producto.nombre}</h5>
+            </div>
+            <div class="modal-body">
                <div>
                   <img src="${producto.img}" class="img-fluid" width="350">
-                  <p><strong>MEDIDAS</strong>${producto.medidads}</p>
-                  <p><strong>MATERIALES</strong>${producto.materiales}</p>
+                  <p><strong>Medidas: </strong>${producto.medidads}</p>
+                  <p><strong>Materiales: </strong>${producto.materiales}</p>
                </div>
-
            </div>
-    
          </div>
        </div>
      </div>
