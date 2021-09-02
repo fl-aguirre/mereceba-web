c616913acbc4ea17294e47579de5b3d6027a4fcb
Franco Aguirre <frl.aguirre@gmail.com>
2021-09-02 17:41:27 -0300

    Spinner, animaciones y optimización de imágenes

---
+++ js/spinner.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var contenedorSpinner = $("#contenedor__carga");
+    $('#fade__effect').hide();
+    contenedorSpinner.hide();
+    $('#fade__effect').fadeIn(1000);
+}
