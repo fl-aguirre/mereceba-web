1ef3e8d51bd691bd44cc096f01b131cd93a76c24
Franco Aguirre <frl.aguirre@gmail.com>
2021-09-03 12:52:23 -0300

    Incorporación de nuevos productos (bombillas), datos de contacto y detalles de estilo

--- js/productos.js
+++ js/productos.js
@@ -1,29 +1,31 @@
-const lista = [];
-
 class Producto {
-    constructor(id, nombre, img, medidas, materiales) {
+    constructor(id, nombre, img, materiales) {
         this.id = id;
         this.nombre = nombre;
         this.img = img;
-        this.medidads = medidas;
         this.materiales = materiales;
     }
 }
 
-function productos() {
-    $.getJSON("./data/data.json",
+function productos(data) {
+    $.getJSON(data,
         function (productos) {
+            const lista = [];
             for (const producto of productos) {
-                lista.push(new Producto(producto.id, producto.nombre, producto.img, producto.medidas, producto.materiales));
+                lista.push(new Producto(producto.id, producto.nombre, producto.img, producto.materiales));
             }
-            renderCard(lista)
+            if (lista[0].id <= 100){
+              renderMates(lista)
+            }else{
+              renderBombillas(lista)
+            }
         }
     );
 }
 
-function renderCard(listado) {
+function renderMates(listado) {
     for (const producto of listado) {
-        $("#productos").append(`
+        $("#productosMates").append(`
         <div class="card productos__card--hover">
           <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#modal${producto.id}">
             <img src=${producto.img} class="card-img-top" alt=${producto.nombre}>      
@@ -39,7 +41,7 @@ function renderCard(listado) {
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               <h5 class="modal-title" id="${producto.id}ModalLabel">${producto.nombre}</h5>
             </div>
-            <div class="modal-body">
+            <div class="modal-body-mate">
                <div>
                   <img src="${producto.img}" class="img-fluid" width="350">
                   <p><strong>Materiales: </strong>${producto.materiales}</p>
@@ -54,4 +56,37 @@ function renderCard(listado) {
     console.log(listado);
 }
 
-productos();
+function renderBombillas(listado) {
+  for (const producto of listado) {
+      $("#productosBombillas").append(`
+      <div class="card productos__card--hover">
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#modal${producto.id}">
+          <img src=${producto.img} class="card-img-top" alt=${producto.nombre}>      
+        </button>
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>    
+        </div>
+     </div>
+     <div class="modal fade" id="modal${producto.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h5 class="modal-title" id="${producto.id}ModalLabel">${producto.nombre}</h5>
+          </div>
+          <div class="modal-body-bombilla">
+             <div>
+                <img src="${producto.img}" class="img-fluid" width="300">
+             </div>
+         </div>
+       </div>
+     </div>
+   </div>
+`);
+
+  }
+  console.log(listado);
+}
+
+productos("./data/dataMates.json");
+productos("./data/dataBombillas.json");
